460e46d4ab5e9822fad1b09cdca703b5139c5749
Anton Ždanov <anton@azdanov.dev>
2025-08-03 11:16:04 +0300

    feat(lib): add query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 
 import {Geist, Geist_Mono} from 'next/font/google';
 
+import {QueryProvider} from '@/components/query-provider';
 import {ThemeProvider} from '@/components/theme-provider';
 
 import './globals.css';
@@ -39,7 +40,7 @@ export default function RootLayout({
           disableTransitionOnChange
           enableSystem
         >
-          {children}
+          <QueryProvider>{children}</QueryProvider>
         </ThemeProvider>
       </body>
     </html>

545371320d0ad95e05e457c371a8085d0097647c
Anton Ždanov <anton@azdanov.dev>
2025-09-14 17:46:44 +0300

    feat(api): add files endpoint

---
+++ src/app/api/search/package/[repo]/[arch]/[pkgname]/files/route.ts
@@ -0,0 +1,26 @@
+import {NextRequest, NextResponse} from 'next/server';
+
+import {getPackageFiles} from '@/lib/actions';
+import {PackageArch, PackageDetailsPathParamsSchema} from '@/lib/types';
+
+export async function GET(
+  _req: NextRequest,
+  ctx: RouteContext<'/api/search/package/[repo]/[arch]/[pkgname]/files'>
+) {
+  const validation = PackageDetailsPathParamsSchema.safeParse(await ctx.params);
+  if (!validation.success) {
+    return NextResponse.json({error: 'Invalid parameters'}, {status: 400});
+  }
+  let {arch, pkgname, repo} = validation.data;
+  arch = decodeURIComponent(arch) as PackageArch;
+  pkgname = decodeURIComponent(pkgname);
+  repo = decodeURIComponent(repo);
+
+  const files = await getPackageFiles({arch, pkgname, repo});
+
+  if (!files) {
+    return NextResponse.json({error: 'Files not found'}, {status: 404});
+  }
+
+  return NextResponse.json(files);
+}

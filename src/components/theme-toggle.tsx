74cb7b06c1c47e48b7ece2b1dc99241a19ea1ddd
Anton Ždanov <anton@azdanov.dev>
2026-01-15 10:24:34 +0200

    feat: update theme switcher icon style

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -18,8 +18,8 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size="icon" variant="ghost">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Sun className="block dark:hidden size-5" />
+          <Moon className="hidden dark:block size-5" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>

54f55b7616afbe3410985fa8f4ed81082ab8f579
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-04-20 01:38:27 +0400

    eslint: add perfectionist and react plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,28 @@
 import pluginQuery from '@tanstack/eslint-plugin-query';
+import perfectionist from 'eslint-plugin-perfectionist';
+import pluginReact from 'eslint-plugin-react';
 import {defineConfig} from 'eslint/config';
 
-export default defineConfig([...pluginQuery.configs['flat/recommended']]);
+export default defineConfig([
+  {
+    ignores: ['**/dist/**', '**/.output/**'],
+  },
+  {
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      'react/display-name': 'warn',
+      'react/no-deprecated': 'error',
+      'react/no-direct-mutation-state': 'error',
+      'react/no-find-dom-node': 'error',
+      'react/no-is-mounted': 'error',
+      'react/no-string-refs': 'error',
+    },
+    settings: {
+      react: {version: 'detect'},
+    },
+  },
+  ...pluginQuery.configs['flat/recommended'],
+  perfectionist.configs['recommended-natural'],
+]);

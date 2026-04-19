f622186da5d0cd0a42118cae47b3236a71afc8d7
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-04-20 01:27:46 +0400

    chore: migrate build tooling from Next.js to TanStack Start

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,4 @@
 import pluginQuery from '@tanstack/eslint-plugin-query';
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTs from 'eslint-config-next/typescript';
-import perfectionist from 'eslint-plugin-perfectionist';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import {defineConfig} from 'eslint/config';
 
-export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  ...pluginQuery.configs['flat/recommended'],
-  perfectionist.configs['recommended-natural'],
-  eslintPluginPrettierRecommended,
-]);
+export default defineConfig([...pluginQuery.configs['flat/recommended']]);

77cf980e9fe331afecd0f1c5f6fb3ddd1bd6feb3
Anton Ždanov <anton@azdanov.dev>
2026-04-21 15:36:56 +0300

    refactor: use zod to set defaults for index page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import {createFileRoute} from '@tanstack/react-router';
+import {createFileRoute, stripSearchParams} from '@tanstack/react-router';
 import {z} from 'zod';
 
 import PackageSearch from '@/components/PackageSearch';
@@ -7,23 +7,33 @@ import {Card, CardContent} from '@/components/ui/card';
 import {searchQueryFn} from '@/lib/query-actions';
 import {PAGE_SIZE, type PackagesSearchQueryParams} from '@/lib/types';
 
+const defaultValues = {
+  arch: '',
+  current_page: 1,
+  page_size: PAGE_SIZE[0],
+  repo: '',
+  search: '',
+};
+
 const SearchParamsSchema = z.object({
-  arch: z.string().optional(),
-  current_page: z.coerce.number().int().positive().optional(),
-  page_size: z.union(PAGE_SIZE.map(size => z.literal(size))).optional(),
-  repo: z.string().optional(),
-  search: z.string().optional(),
+  arch: z.string().catch(defaultValues.arch),
+  current_page: z.number().int().positive().catch(defaultValues.current_page),
+  page_size: z
+    .union(PAGE_SIZE.map(size => z.literal(size)))
+    .catch(defaultValues.page_size),
+  repo: z.string().catch(defaultValues.repo),
+  search: z.string().catch(defaultValues.search),
 });
 
 type SearchParams = z.infer<typeof SearchParamsSchema>;
 
 function toQueryParams(search: SearchParams): PackagesSearchQueryParams {
   return {
-    arch: search.arch ?? '',
-    current_page: search.current_page ?? 1,
-    page_size: search.page_size ?? PAGE_SIZE[0],
-    repo: search.repo ?? '',
-    search: search.search ?? '',
+    arch: search.arch,
+    current_page: search.current_page,
+    page_size: search.page_size,
+    repo: search.repo,
+    search: search.search,
   };
 }
 
@@ -31,14 +41,18 @@ export const Route = createFileRoute('/')({
   component: HomePage,
   head: () => ({meta: [{title: 'CachyOS | Package Search'}]}),
   loader: ({context: {queryClient}, deps}) => {
+    // TODO: Figure out why deps is '{}' type
     const params = toQueryParams(deps as SearchParams);
     return queryClient.prefetchQuery({
       queryFn: searchQueryFn(params),
       queryKey: ['search', params],
     });
   },
-  loaderDeps: ({search}): SearchParams => search,
+  loaderDeps: ({search}) => search,
   validateSearch: SearchParamsSchema,
+  search: {
+    middlewares: [stripSearchParams(defaultValues)],
+  },
 });
 
 function HomePage() {

08ddf1c21feb2fbe77e7b1e54233bebb8e0b4d0d
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2026-04-22 20:09:33 +0400

    format: run biome check

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {Moon, Sun, Check} from 'lucide-react';
+import {Check, Moon, Sun} from 'lucide-react';
 import {useTheme} from 'next-themes';
 
 import {Button} from '@/components/ui/button';

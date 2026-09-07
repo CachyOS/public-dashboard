12bffd99ef1707197a9abdcabc3eec9ba4585eac
Autumn McKee <autumn.mckee@cern.ch>
2026-09-07 13:43:14 +0200

    wrap-anywhere instead of break-words, remove unnececary max-w-full

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,7 +5,7 @@ import type * as React from 'react';
 import {cn} from '@/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit max-w-full shrink-0 break-words [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  'inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit shrink-0 wrap-anywhere [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
   {
     defaultVariants: {
       variant: 'default',
